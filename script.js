9325fb4fe1c3383321fe9ffb79153e5f1ed04760
Vruhali Murlidhar Patil <vrushalimpatil2001@gmail.com>
2022-12-31 00:26:04 +0530

    completed

---
+++ script.js
@@ -0,0 +1,14 @@
+function submitForm(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let reason = document.getElementById("reason").value;
+
+    console.log(fname, lname, email, reason);
+
+    if(fname != null && lname != null && email != null && reason != null)
+    {
+        alert("Form Submitted Successfully")
+    }
+
+}
